b6d8191a9efcc74961ed8a9d0f0a50ebd4782bc0
freeway3800 <freedom3800@daum.net>
2022-01-07 06:41:53 +0900

    django web main start

--- pro_1/app_1/views.py
+++ pro_1/app_1/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def main_home(request):
+    return render(request, '../templates/base.html')
+
+
+"""
+from django.http import HttpResponse
+def main_home(request):
+    return HttpResponse("Hello, world. You're at the polls index.")"""
