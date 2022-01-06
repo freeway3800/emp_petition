1b9d1d364191e2d5936cbf7128e10e7c3ce55be9
freeway3800 <freedom3800@daum.net>
2022-01-07 04:53:37 +0900

    data_download_start

--- 1_data_download.py
+++ 1_data_download.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import datetime
 import time
 import re
+import pandas as pd
 import sys
 
+# 2일 간격 한번 실행
+# 데이터 다운로드 후 정리
 """ class 데이터 추출.
     1. 제목
     2. 동의수
@@ -11,9 +15,9 @@ import sys
     -->pandas-> 데이터 분석 -> 제목/ 추천수 검색
 
 """
-document = []
-for page in range(1, 3, 1):
-    sys.stdout = open('df.txt', 'w')
+df_txt= []
+df_link_txt=[]
+for page in range(1, 5, 1):
     url = "https://www1.president.go.kr/petitions/best?page={}".format(page)
     html = urlopen(url)
     petition = BeautifulSoup(html, 'html.parser')
@@ -37,23 +41,19 @@ for page in range(1, 3, 1):
             qua = qua_search.findall(str(df_qua))
             qua = int(qua[0] + qua[1])
             name = ds.strip('\n 제목 ')
-            print(name,qua,sep='\t')
 
             # 리스트 만들기
             doc_dict = {'name': name, 'qua': qua}
-            document.append(doc_dict)
+            df_txt.append(doc_dict)
 
         except:
             print('')
 
-    sys.stdout = open('df_link.txt', 'w')
     for i in range(0, len(links), 1):
         link = "https://www1.president.go.kr/petitions/{}?navigation=best".format(links[i])
-        print(link)
+        df_link_txt.append(link)
 
-sys.stdout.close()
-
-
-
-
-
+df=pd.DataFrame(df_txt)
+df['link']= df_link_txt
+df.columns=["제목","청원수","링크"]
+df.to_csv('df.csv')
